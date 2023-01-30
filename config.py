f7def11a98b3998ecaa51a5f048cccfba6f8fe8e
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-01-30 13:06:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+APP_ID = 
+API_HASH = 
+BOT_TOKEN = 
+BOT_TOKEN2 =
+BOT_TOKEN3 = 
+BOT_TOKEN4 =
+BOT_TOKEN5 =
+BOT_TOKEN6 =
+BOT_TOKEN7 =
+BOT_TOKEN8 =
+BOT_TOKEN9 =
+BOT_TOKEN10 =
+
+SUDO_USERS = 
+
+bot = TelegramClient('LegendBoy', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
+bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
+bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
+bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
+bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
+bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
+bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
+bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
+bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
+bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
+
