7ee1073069ec743cd6a9e39720b624f0aa094adb
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-16 02:53:47 +0000

    legend fixed

--- LegendGirl/core/clients.py
+++ LegendGirl/core/clients.py
@@ -1,31 +1,53 @@
-#starting all clients
+# starting all clients
 from LegendGirl.Config import *
 
-
-if  ":" in BOT_TOKEN:
-   Client1 = Client('LegendSpam', api_id=APP_ID, api_hash=API_HASH, bot_token=BOT_TOKEN, plugins=dict(root="LegendGirl.LegendBoy"))
-   print("LegendSpam : Bot token 1 has been found")
+if ":" in BOT_TOKEN:
+    Client1 = Client(
+        "LegendSpam",
+        api_id=APP_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN,
+        plugins=dict(root="LegendGirl.LegendBoy"),
+    )
+    print("LegendSpam : Bot token 1 has been found")
 else:
-   print("LegendSpam : Client 1 not Found")
-
+    print("LegendSpam : Client 1 not Found")
 
 
 if BOT_TOKEN2:
-   Client2 = Client('LegendSpam2', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN2, plugins=dict(root="LegendGirls.LegendBoy"))
-   print("LegendSpam : Bot token 2 Found")
+    Client2 = Client(
+        "LegendSpam2",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN2,
+        plugins=dict(root="LegendGirls.LegendBoy"),
+    )
+    print("LegendSpam : Bot token 2 Found")
 else:
-   Client2 = None
-         
-   
+    Client2 = None
+
+
 if BOT_TOKEN3:
-   Client3 = Client('LegendSpam3', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN3, plugins=dict(root="LegendGirls.LegendBoy"))
-   print("LegendSpam : Bot token 3 Found")
+    Client3 = Client(
+        "LegendSpam3",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN3,
+        plugins=dict(root="LegendGirls.LegendBoy"),
+    )
+    print("LegendSpam : Bot token 3 Found")
 else:
-   Client3 = None
-         
+    Client3 = None
+
 
 if BOT_TOKEN4:
-    Client4 = Client('LegendSpam4', api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN4, plugins=dict(root="LegendGirls.LegendBoy"))
+    Client4 = Client(
+        "LegendSpam4",
+        api_id=API_ID,
+        api_hash=API_HASH,
+        bot_token=BOT_TOKEN4,
+        plugins=dict(root="LegendGirls.LegendBoy"),
+    )
     print("LegendSpam - [INFO]: Bot token 4 Found")
 else:
-   Client4 = None
+    Client4 = None
