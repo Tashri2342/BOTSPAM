3bade83c400ecfbd6132cb51b61c3ee25515cd1f
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-19 23:31:22 +0530

    Update exampleconfig.py

--- exampleconfig.py
+++ exampleconfig.py
@@ -1,5 +1,3 @@
-from telethon import TelegramClient
-
 APP_ID = 
 API_HASH = 
 BOT_TOKEN = 
@@ -14,15 +12,3 @@ BOT_TOKEN9 =
 BOT_TOKEN10 =
 
 SUDO_USERS = 
-
-bot = TelegramClient('LegendBoy', APP_ID, API_HASH).start(bot_token=BOT_TOKEN) 
-bot2 = TelegramClient('LegendBoy2', APP_ID, API_HASH).start(bot_token=BOT_TOKEN2) 
-bot3 = TelegramClient('LegendBoy3', APP_ID, API_HASH).start(bot_token=BOT_TOKEN3) 
-bot4 = TelegramClient('LegendBoy4', APP_ID, API_HASH).start(bot_token=BOT_TOKEN4) 
-bot5 = TelegramClient('LegendBoy5', APP_ID, API_HASH).start(bot_token=BOT_TOKEN5) 
-bot6 = TelegramClient('LegendBoy6', APP_ID, API_HASH).start(bot_token=BOT_TOKEN6) 
-bot7 = TelegramClient('LegendBoy7', APP_ID, API_HASH).start(bot_token=BOT_TOKEN7) 
-bot8 = TelegramClient('LegendBoy8', APP_ID, API_HASH).start(bot_token=BOT_TOKEN8) 
-bot9 = TelegramClient('LegendBoy9', APP_ID, API_HASH).start(bot_token=BOT_TOKEN9) 
-bot10 = TelegramClient('LegendBoy10', APP_ID, API_HASH).start(bot_token=BOT_TOKEN10)
-
