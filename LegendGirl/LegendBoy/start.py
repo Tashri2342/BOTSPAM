caaf8269ec3ec1f25a3d028eee885de99a464b07
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 18:47:08 +0000

    legend fixed

--- LegendGirl/LegendBoy/start.py
+++ LegendGirl/LegendBoy/start.py
@@ -1,10 +1,9 @@
-from pyrogram import Client, filters
 from LegendBoy.Config import *
+from pyrogram import Client, filters
 
 handler = HANDLER
 
+
 @Client.on_message(filters.me & filters.command(["start"], prefixes=handler))
-async def start(Legend: Client, message: Message):       
-    await Legend.send_message(
-             message.chat.id, 
-             "Hello")
+async def start(Legend: Client, message: Message):
+    await Legend.send_message(message.chat.id, "Hello")
