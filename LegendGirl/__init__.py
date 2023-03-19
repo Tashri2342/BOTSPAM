d763e7fb566c832e6e1366701b119747ba700fb8
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 17:16:25 +0000

    legend fixed

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -9,7 +9,7 @@ start_time = time.time()
 
 sudouser = []
 if SUDO_USERS:
-    sudouser = str(SUDO_USERS).split(' ')
+    sudouser = str(SUDO_USERS).split(" ")
     _list = []
     for x in sudouser:
         _list.append(int(x))
