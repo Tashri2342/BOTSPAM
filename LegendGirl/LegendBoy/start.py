d4d0d0f16a6fc5ec6415e07d8231f54b213a182c
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 19:22:33 +0000

    legend fixed

--- LegendGirl/LegendBoy/start.py
+++ LegendGirl/LegendBoy/start.py
@@ -5,6 +5,7 @@ from LegendGirl.Config import *
 
 from .. import sudos
 
+
 @Client.on_message(filters.user(sudos) & filters.command(["start"], prefixes=HANDLER))
 async def start(Legend: Client, message: Message):
     await Legend.send_photo(message.chat.id, ALIVE_PIC, caption="Hello")
