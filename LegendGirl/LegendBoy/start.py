0eb024d3e2c3037ae619d228bae97f8cd6209f4c
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-20 00:52:16 +0530

    Update start.py

--- LegendGirl/LegendBoy/start.py
+++ LegendGirl/LegendBoy/start.py
@@ -5,10 +5,6 @@ from LegendGirl.Config import *
 
 from .. import sudos
 
-handler = HANDLER
-ALIVE_PIC = "https://te.legra.ph/file/315ec5fe03f2a612139b3.jpg"
-
-
-@Client.on_message(filters.user(sudos) & filters.command(["start"], prefixes=handler))
+@Client.on_message(filters.user(sudos) & filters.command(["start"], prefixes=HANDLER))
 async def start(Legend: Client, message: Message):
     await Legend.send_photo(message.chat.id, ALIVE_PIC, caption="Hello")
