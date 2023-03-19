584464d927f0d93110f0b7779b74cf484eb74624
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-19 22:46:09 +0530

    Update __init__.py

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -5,13 +5,11 @@ from .core import *
 from .Resources import *
 
 start_time = time.time()
-version = "v1.0"
-group_username = "@LegendUserBot_XD"
 # Sudo Users
 
 sudouser = []
 if SUDO_USERS:
-    sudouser = str(SUDO_USERS).split(" ")
+    sudouser = str(SUDO_USERS).split(' ')
     _list = []
     for x in sudouser:
         _list.append(int(x))
