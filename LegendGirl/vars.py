68101a02a859d44c053b55eb937fa477e1b1d888
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 18:35:31 +0000

    legend fixed

--- LegendGirl/vars.py
+++ LegendGirl/vars.py
@@ -1,25 +1,24 @@
-from exampleconfig import *
 import os
 
+from exampleconfig import *
 
 if BOT_TOKEN == "":
-  pass
+    pass
 else:
-  add_values()
-
+    add_values()
 
 
 def add_values():
-    APP_ID = os.environ.get("APP_ID")
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    BOT_TOKEN2 = os.environ.get("BOT_TOKEN2")
-    BOT_TOKEN3 = os.environ.get("BOT_TOKEN3")
-    BOT_TOKEN4 = os.environ.get("BOT_TOKEN4")
-    BOT_TOKEN5 = os.environ.get("BOT_TOKEN5")
-    BOT_TOKEN6 = os.environ.get("BOT_TOKEN6")
-    BOT_TOKEN7 = os.environ.get("BOT_TOKEN7")
-    BOT_TOKEN8 = os.environ.get("BOT_TOKEN8")
-    BOT_TOKEN9 = os.environ.get("BOT_TOKEN9")
-    BOT_TOKEN10 = os.environ.get("BOT_TOKEN10")
-    SUDO_USERS = os.environ.get("SUDO_USERS")
+    os.environ.get("APP_ID")
+    os.environ.get("API_HASH")
+    os.environ.get("BOT_TOKEN")
+    os.environ.get("BOT_TOKEN2")
+    os.environ.get("BOT_TOKEN3")
+    os.environ.get("BOT_TOKEN4")
+    os.environ.get("BOT_TOKEN5")
+    os.environ.get("BOT_TOKEN6")
+    os.environ.get("BOT_TOKEN7")
+    os.environ.get("BOT_TOKEN8")
+    os.environ.get("BOT_TOKEN9")
+    os.environ.get("BOT_TOKEN10")
+    os.environ.get("SUDO_USERS")
