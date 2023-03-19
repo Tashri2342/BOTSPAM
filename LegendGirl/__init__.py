318a238c3b48a5546dd32ab90544ae10a34f42f7
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-03-19 21:23:38 +0530

    Update __init__.py

---
+++ LegendGirl/__init__.py
@@ -0,0 +1,18 @@
+import time
+
+from .core import *
+from .Config import *
+from .Resources import *
+
+start_time  = time.time()
+
+#Sudo Users
+
+sudoser = []
+if SUDO_USERS:
+  sudoser = SUDO_USERS.split(" ")
+  _list = []
+  for x in sudouser:
+        _list.append(int(x))
+else:
+  print("Add Key Sudo User")
