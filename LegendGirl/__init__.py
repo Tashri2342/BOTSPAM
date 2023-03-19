abed543b231b31a5a4d0b58811514ce6713d5798
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-19 22:39:44 +0530

    Create __init__.py

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -1,18 +1,19 @@
 import time
 
-from .Config import *
 from .core import *
+from .Config import *
 from .Resources import *
 
-start_time = time.time()
-
-# Sudo Users
+start_time  = time.time()
+version = "v1.0"
+group_username = "@LegendUserBot_XD"
+#Sudo Users
 
 sudouser = []
 if SUDO_USERS:
-    sudouser = str(SUDO_USERS).split(" ")
-    _list = []
-    for x in sudouser:
+  sudouser = str(SUDO_USERS).split(" ")
+  _list = []
+  for x in sudouser:
         _list.append(int(x))
 else:
-    print("Add Key Sudo User")
+  print("Add Key Sudo User")
