560e1a60ea101650d2123ae866254c543d1614cf
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 17:43:18 +0000

    legend fixed

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -7,17 +7,17 @@ from .Resources import *
 start_time = time.time()
 # Sudo Users
 
-# full debugging 
+# full debugging
 if SUDO_USERS:
     try:
-      sudouser = str(SUDO_USERS).split(" ")
-      print (sudouser)
-      _list = []
-      for x in sudouser:
-          print (x)
-          _list.append(int(x))
+        sudouser = str(SUDO_USERS).split(" ")
+        print(sudouser)
+        _list = []
+        for x in sudouser:
+            print(x)
+            _list.append(int(x))
     except Exception as e:
-      global SUDO_USERS
-      print(e)
+        global SUDO_USERS
+        print(e)
 else:
     print("Add Key Sudo User")
