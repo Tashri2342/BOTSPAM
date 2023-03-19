938037195fd99a386ed24c11089316392b8c6443
LegendBoy XD <97836367+LEGEND-AI@users.noreply.github.com>
2023-03-19 21:12:55 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,54 +1,5 @@
-import glob
-import logging
-from pathlib import Path
+from . import *
 
-from LegendGirl.utils import load_plugins
-
-from . import bot, bot2, bot3, bot5, bot6, bot7, bot8, bot9, bot10
-
-logging.basicConfig(
-    format="[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s", level=logging.WARNING
-)
-
-path = "LegendGirl/LegendBoy/*.py"
-files = glob.glob(path)
-for name in files:
-    with open(name) as a:
-        patt = Path(a.name)
-        plugin_name = patt.stem
-        load_plugins(plugin_name.replace(".py", ""))
-
-print("Your Legend Spam Bot Has Been Deployed Successfully")
-print(
-    "Congratulations 😇 For Being A Part Of LegendBot \nJoin @LegendBot_OP For Any Help"
-)
-
+     
 if __name__ == "__main__":
-    bot.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot2.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot3.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot4.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot5.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot6.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot7.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot8.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot9.run_until_disconnected()
-
-if __name__ == "__main__":
-    bot10.run_until_disconnected()
+    Start_BotSpam()
