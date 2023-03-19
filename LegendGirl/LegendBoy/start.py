a47e0e559109a88365bd4293c9fd05bd6e03cf2e
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-19 19:07:18 +0000

    legend fixed

--- LegendGirl/LegendBoy/start.py
+++ LegendGirl/LegendBoy/start.py
@@ -9,8 +9,6 @@ handler = HANDLER
 ALIVE_PIC = "https://te.legra.ph/file/315ec5fe03f2a612139b3.jpg"
 
 
-@Client.on_message(
-    filters.user(sudos) & filters.command(["start"], prefixes=handler)
-)
+@Client.on_message(filters.user(sudos) & filters.command(["start"], prefixes=handler))
 async def start(Legend: Client, message: Message):
     await Legend.send_photo(message.chat.id, ALIVE_PIC, caption="Hello")
