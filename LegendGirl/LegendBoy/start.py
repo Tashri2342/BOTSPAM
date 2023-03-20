367e856d5e777f56eea9238312a36491c97e7ce1
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-20 04:24:41 +0000

    legend fixed

--- LegendGirl/LegendBoy/start.py
+++ LegendGirl/LegendBoy/start.py
@@ -6,13 +6,16 @@ from LegendGirl.Config import *
 from .. import sudos
 
 
-
 def start_cmd(Legend):
     x = Legend.get_me()
     START_OP = [
         [
-            InlineKeyboardButton(text="🥀 Developer 🥀", url=f"https://t.me/LegendBot_Owner"),
-            InlineKeyboardButton(text="✨ Support ✨", url=f"https://t.me/LegendBot_Group"),
+            InlineKeyboardButton(
+                text="🥀 Developer 🥀", url=f"https://t.me/LegendBot_Owner"
+            ),
+            InlineKeyboardButton(
+                text="✨ Support ✨", url=f"https://t.me/LegendBot_Group"
+            ),
         ],
         [
             InlineKeyboardButton(
@@ -24,24 +27,27 @@ def start_cmd(Legend):
             InlineKeyboardButton(text="🚀 Helps & Cmds 🚀", callback_data="HELP"),
         ],
         [
-            InlineKeyboardButton(text="❄️ Source Code ❄️", url=f"https://github.com/LEGEND-AI/BOTSPAM"),
-            InlineKeyboardButton(text="☁️ Updates ☁️", url=f"https://t.me/LegendBot_Update"),
+            InlineKeyboardButton(
+                text="❄️ Source Code ❄️", url=f"https://github.com/LEGEND-AI/BOTSPAM"
+            ),
+            InlineKeyboardButton(
+                text="☁️ Updates ☁️", url=f"https://t.me/LegendBot_Update"
+            ),
         ],
     ]
     return START_OP
 
+
 @Client.on_message(filters.user(sudos) & filters.command(["start"], prefixes=HANDLER))
 async def start(Legend: Client, message: Message):
     if ".jpg" in START_PIC or ".png" in START_PIC:
-       await Legend.send_photo(
-             message.chat.id,
-             START_PIC, 
-             caption="trying",
-             reply_markup=InlineKeyboardMarkup(start_cmd(Legend)))
-    elif ".mp4" in START_PIC  or ".MP4," in START_PIC:
-       await Legend.send_video(message.chat.id, 
-             START_PIC, 
-             caption="trying")
+        await Legend.send_photo(
+            message.chat.id,
+            START_PIC,
+            caption="trying",
+            reply_markup=InlineKeyboardMarkup(start_cmd(Legend)),
+        )
+    elif ".mp4" in START_PIC or ".MP4," in START_PIC:
+        await Legend.send_video(message.chat.id, START_PIC, caption="trying")
     else:
         await Legend.send_message(message.chat_id, "trying")
-        
