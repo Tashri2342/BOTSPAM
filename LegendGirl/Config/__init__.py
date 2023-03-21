a80d8391d8ee395f02bf7af4bd45ce4565b8f2b0
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-22 03:37:51 +0530

    Update __init__.py

--- LegendGirl/Config/__init__.py
+++ LegendGirl/Config/__init__.py
@@ -2,7 +2,11 @@ import os
 
 ENV = bool(os.environ.get("ENV", False))
 
-if ENV:
-    from sample_config import *  # noqa
-elif os.path.exists("config.py"):
-    from config import *  # noqa
+async def config(ENV):
+    if ENV:
+        from sample_config import *  # noqa
+    elif os.path.exists("config.py"):
+        from config import *  # noqa
+    return *
+
+Config = config(ENV)
