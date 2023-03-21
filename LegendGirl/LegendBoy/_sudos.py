2c47a91c02c770cdaeb85e236b940c09fad758cd
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-21 17:10:25 +0000

    legend fixed

--- LegendGirl/LegendBoy/_sudos.py
+++ LegendGirl/LegendBoy/_sudos.py
@@ -1,31 +1,37 @@
-import os, sys
-from .. import sudos
+from LegendBoy.Config import *
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-from LegendBoy.Config import *
+from .. import sudos
+
 
 async def get_user(Legend, message):
     try:
-       args = message.text.split(" ", 1)[1].split(" ", 1)
+        args = message.text.split(" ", 1)[1].split(" ", 1)
     except IndexError:
-       args = None
+        args = None
     if message.reply_to_message and message.reply_to_message.from_user:
-       user = message.reply_to_message.from_user
+        user = message.reply_to_message.from_user
     elif args:
-       user_ = args[0]
-       if user_.isnumeric():
-           user_ = int(user_)
-       if not user_:
-           await message.reply_text("I don't know who you're talking about, you're going to need to specify a user.!")
-           return
-       try:
-           user = await Legend.get_users(user_)
-       except (TypeError, ValueError):
-           await message.reply_text("Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them.")
-           return
+        user_ = args[0]
+        if user_.isnumeric():
+            user_ = int(user_)
+        if not user_:
+            await message.reply_text(
+                "I don't know who you're talking about, you're going to need to specify a user.!"
+            )
+            return
+        try:
+            user = await Legend.get_users(user_)
+        except (TypeError, ValueError):
+            await message.reply_text(
+                "Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them."
+            )
+            return
     else:
-        await message.reply_text("I don't know who you're talking about, you're going to need to specify a user...!")
+        await message.reply_text(
+            "I don't know who you're talking about, you're going to need to specify a user...!"
+        )
         return
 
     return user
@@ -34,13 +40,13 @@ async def get_user(Legend, message):
 @Client.on_message(filters.me & filters.command(["addsudo"], prefixes=HANDLER))
 async def addsudo(Legend: Client, message: Message):
     try:
-       user = await get_user(Legend, message)
+        user = await get_user(Legend, message)
     except Exception as e:
-       await message.reply(user_errors(e))
-       return
+        await message.reply(user_errors(e))
+        return
     if int(user.id) in sudos:
-       await message.reply_text(f"User {user.mention} already in sudo list!")
-       return
+        await message.reply_text(f"User {user.mention} already in sudo list!")
+        return
     sudos.append(user.id)
     await message.reply_text(f"User {user.mention} successfully promoted as Sudo!")
 
@@ -48,24 +54,26 @@ async def addsudo(Legend: Client, message: Message):
 @Client.on_message(filters.me & filters.command(["rmsudo"], prefixes=HANDLER))
 async def remsudo(Legend: Client, message: Message):
     try:
-       user = await get_user(Legend, message)
+        user = await get_user(Legend, message)
     except Exception as e:
-       await message.reply(user_errors(e))
-       return
+        await message.reply(user_errors(e))
+        return
     if int(user.id) not in sudos:
-       await message.reply_text(f"User {user.mention} not in sudo list!")
-       return
+        await message.reply_text(f"User {user.mention} not in sudo list!")
+        return
     sudos.remove(user.id)
     await message.reply_text(f"User {user.mention} successfully removed from Sudo!")
 
-@Client.on_message(filters.me & filters.command(["sudos", "sudolist"], prefixes=handler))
+
+@Client.on_message(
+    filters.me & filters.command(["sudos", "sudolist"], prefixes=handler)
+)
 async def sudolist(Legend: Client, message: Message):
     sudo_reply = "**Sudo users list - SpamX** \n\n"
     for x in sudos:
-       try:
-          user = await Legend.get_users(x)
-          sudo_reply += f" × {user.mention} \n"
-       except:
-          sudo_reply += f" × [{x}](tg://user?id={x}) \n"
+        try:
+            user = await Legend.get_users(x)
+            sudo_reply += f" × {user.mention} \n"
+        except:
+            sudo_reply += f" × [{x}](tg://user?id={x}) \n"
     await message.reply_text(sudo_reply)
-
