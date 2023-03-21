df0c844a77cc173576790db747574dee4c4e3ef9
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-22 03:52:55 +0530

    Update _Sudos.py

--- LegendGirl/LegendBoy/_Sudos.py
+++ LegendGirl/LegendBoy/_Sudos.py
@@ -4,39 +4,7 @@ from pyrogram.types import Message
 from LegendGirl.Config import *
 
 from .. import sudos
-
-
-async def get_user(Legend, message):
-    try:
-        args = message.text.split(" ", 1)[1].split(" ", 1)
-    except IndexError:
-        args = None
-    if message.reply_to_message and message.reply_to_message.from_user:
-        user = message.reply_to_message.from_user
-    elif args:
-        user_ = args[0]
-        if user_.isnumeric():
-            user_ = int(user_)
-        if not user_:
-            await message.reply_text(
-                "I don't know who you're talking about, you're going to need to specify a user.!"
-            )
-            return
-        try:
-            user = await Legend.get_users(user_)
-        except (TypeError, ValueError):
-            await message.reply_text(
-                "Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them."
-            )
-            return
-    else:
-        await message.reply_text(
-            "I don't know who you're talking about, you're going to need to specify a user...!"
-        )
-        return
-
-    return user
-
+from LegendBS.get_user import get_user
 
 @Client.on_message(filters.me & filters.command(["addsudo"], prefixes=HANDLER))
 async def addsudo(Legend: Client, message: Message):
