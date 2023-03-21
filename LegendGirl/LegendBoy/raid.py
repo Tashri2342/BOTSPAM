92627645dea00a4adeb1a1bb0fedfe5de5e8cd9e
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-21 15:06:36 +0000

    legend fixed

--- LegendGirl/LegendBoy/raid.py
+++ LegendGirl/LegendBoy/raid.py
@@ -67,26 +67,32 @@ async def replyraid(Legend: Client, e: Message):
     global users
     usage: "/re"
     try:
-       lol = e.text.split(" ", 1)[1].split(" ", 1)
+        lol = e.text.split(" ", 1)[1].split(" ", 1)
     except IndexError:
-       lol = None
+        lol = None
     if e.reply_to_message and e.reply_to_message.from_user:
-       user = e.reply_to_message.from_user
+        user = e.reply_to_message.from_user
     elif lol:
         user_ = lol[0]
         if user_.isnumeric():
             user_ = int(user_)
         if not user_:
-            await message.reply_text("I don't know who you're talking about, you're going to need to specify a user.!")
+            await message.reply_text(
+                "I don't know who you're talking about, you're going to need to specify a user.!"
+            )
             return
         try:
             user = await Legend.get_users(user_)
-        except(TypeError, ValueError):
-            await message.reply_text("Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them.")
+        except (TypeError, ValueError):
+            await message.reply_text(
+                "Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them."
+            )
             return
     else:
-        await message.reply_text("I don't know who you're talking about, you're going to need to specify a user...!")
-        return 
+        await message.reply_text(
+            "I don't know who you're talking about, you're going to need to specify a user...!"
+        )
+        return
     if int(user.id) in users:
         await e.reply_text("User already in Raid list!")
         return
@@ -109,26 +115,32 @@ async def replyraid(Legend: Client, e: Message):
 async def draid(Legend: Client, e: Message):
     global users
     try:
-       lol = e.text.split(" ", 1)[1].split(" ", 1)
+        lol = e.text.split(" ", 1)[1].split(" ", 1)
     except IndexError:
-       lol = None
+        lol = None
     if e.reply_to_message and e.reply_to_message.from_user:
-       user = e.reply_to_message.from_user
+        user = e.reply_to_message.from_user
     elif lol:
         user_ = lol[0]
         if user_.isnumeric():
             user_ = int(user_)
         if not user_:
-            await message.reply_text("I don't know who you're talking about, you're going to need to specify a user.!")
+            await message.reply_text(
+                "I don't know who you're talking about, you're going to need to specify a user.!"
+            )
             return
         try:
             user = await Legend.get_users(user_)
-        except(TypeError, ValueError):
-            await message.reply_text("Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them.")
+        except (TypeError, ValueError):
+            await message.reply_text(
+                "Looks like I don't have control over that user, or the ID isn't a valid one. If you reply to one of their messages, I'll be able to interact with them."
+            )
             return
     else:
-        await message.reply_text("I don't know who you're talking about, you're going to need to specify a user...!")
-        return 
+        await message.reply_text(
+            "I don't know who you're talking about, you're going to need to specify a user...!"
+        )
+        return
     if int(user.id) not in users:
         await e.reply_text("User not in Raid list!")
         return
