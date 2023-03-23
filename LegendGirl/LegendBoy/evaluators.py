adaecfdd913062522e721fccc52700c73c8e678b
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-23 02:14:31 +0000

    legend fixed

--- LegendGirl/LegendBoy/evaluators.py
+++ LegendGirl/LegendBoy/evaluators.py
@@ -4,7 +4,8 @@ import sys
 from io import StringIO
 
 from pyrogram import Client, filters
-from pyrogram.types import Message 
+from pyrogram.types import Message
+
 from LegendGirl.Config import *
 
 from .. import sudos
