38529a58a7cfa7a5b70922266fa7a07dd289649f
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-23 05:23:08 +0000

    legend fixed

--- LegendGirl/LegendBoy/banall.py
+++ LegendGirl/LegendBoy/banall.py
@@ -1,6 +1,3 @@
-import asyncio
-from random import choice
-
 from LegendBS.banall import start_banall
 from pyrogram import Client, filters
 from pyrogram.types import Message
@@ -9,10 +6,10 @@ from LegendGirl.Config import *
 
 from .. import sudos
 
+
 @Client.on_message(filters.user(sudos) & filters.command(["raid"], prefixes=HANDLER))
 async def banall(Legend: Client, message: Message):
-   if message.chat.id == message.from_user.id:
-     await message.reply_text("Use this CMD in group;")
-     return
-   await start_banall(Legend, message)
-
+    if message.chat.id == message.from_user.id:
+        await message.reply_text("Use this CMD in group;")
+        return
+    await start_banall(Legend, message)
