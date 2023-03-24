0aecf5d2ca963ea74cfcc3b07c0d542a107f14ca
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-03-24 11:12:07 +0000

    legend fixed

--- LegendGirl/LegendBoy/core.py
+++ LegendGirl/LegendBoy/core.py
@@ -1,32 +1,45 @@
-import os, sys, asyncio, datetime, time
-from .. import sudos
-from LegendGirl import start_time
-from LegendGirl.Config import *
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from pyrogram.enums import ChatType
+import datetime
+import os
+import sys
+import time
 
 from LegendBS.get_time import get_time
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from LegendGirl import start_time
+from LegendGirl.Config import *
+
+from .. import sudos
+
 
 @Client.on_message(filters.user(sudos) & filters.command(["ping"], prefixes=HANDLER))
-async def ping(_, e: Message):       
-      start = datetime.datetime.now()
-      uptime = await get_time((time.time() - start_time))
-      pong_msg = await e.reply("**Pong !!**")
-      end = datetime.datetime.now()
-      ms = (end-start).microseconds / 1000
-      await pong_msg.edit_text(f" {ping_msg} \n\n ༝ ᴘɪɴɢ: `{ms}` ᴍs \n ༝ ᴜᴘᴛɪᴍᴇ: `{uptime}`")
-     
-@Client.on_message(filters.user(sudos) & filters.command(["restart", "reboot"], prefixes=HANDLER))
-async def restarter(Legend: Client, message: Message):
-   await message.reply_text("**Re-starting...** \n Please wait!")
-   try:
-     await Legend.stop()
-   except Exception as error:
-     print(str(error))
+async def ping(_, e: Message):
+    start = datetime.datetime.now()
+    uptime = await get_time((time.time() - start_time))
+    pong_msg = await e.reply("**Pong !!**")
+    end = datetime.datetime.now()
+    ms = (end - start).microseconds / 1000
+    await pong_msg.edit_text(
+        f" {ping_msg} \n\n ༝ ᴘɪɴɢ: `{ms}` ᴍs \n ༝ ᴜᴘᴛɪᴍᴇ: `{uptime}`"
+    )
 
-   args = [sys.executable, "-m", "LegendGirl"]
-   os.execl(sys.executable, *args)
-   quit()
-   
-await message.reply_text(f"**Wrong Usage** \n Syntax: {HANDLER}setvar (var name) (value)")
+
+@Client.on_message(
+    filters.user(sudos) & filters.command(["restart", "reboot"], prefixes=HANDLER)
+)
+async def restarter(Legend: Client, message: Message):
+    await message.reply_text("**Re-starting...** \n Please wait!")
+    try:
+        await Legend.stop()
+    except Exception as error:
+        print(str(error))
+
+    args = [sys.executable, "-m", "LegendGirl"]
+    os.execl(sys.executable, *args)
+    quit()
+
+
+await message.reply_text(
+    f"**Wrong Usage** \n Syntax: {HANDLER}setvar (var name) (value)"
+)
