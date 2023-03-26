6954e81a1c3730e34573817da3b7725190a48910
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-26 15:55:14 +0530

    Update evaluators.py

--- LegendGirl/LegendBoy/evaluators.py
+++ LegendGirl/LegendBoy/evaluators.py
@@ -33,11 +33,12 @@ async def _eval(Legend: Client, message: Message):
 
     result = sys.stdout = StringIO()
     try:
+        lol = exec(f"{code}")
         await message.reply_text(
             f"<b>Code:</b>\n"
             f"<code>{code}</code>\n\n"
             f"<b>Result</b>:\n"
-            f"exec{code}"
+            f"<code>{lol}</code>
         )
     except:
         await message.reply_text(
