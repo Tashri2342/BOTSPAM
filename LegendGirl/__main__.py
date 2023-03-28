98859f8acf54863887aef9d97f23d1696d95a3d6
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-28 10:15:50 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,8 +1,28 @@
-from .core.clients import *
+import asyncio
+from pyrogram import Client
+from LegendGirl.Config import *
+
+tokens = []
+for i in tokens:
+    tokens.append(BOT_TOKEN2)
+
+Client1 = Client(
+    "LegendSpam",
+    api_id=APP_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="LegendGirl.LegendBoy"),
+)
+
+clients = [Client1]
+i = 0
+for token in tokens:
+  if ":" in token:
+   i = i+1
+   Client2 = Client(f"app{i}",bot_token = f"{token}",api_id = api_id,api_hash = api_hash, plugins=plugins)
+   clients.append(Client2)
 
 async def main():
-    apps = [Client1, Client2, Client3, Client4]
-    compose([apps])
-
+  await compose(clients)
 
 Client1.run(main())
