d846079ac4f64533436d62905c4601dda906bf17
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-28 09:42:20 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,7 +1,8 @@
 from .core.clients import *
 
 async def main():
-  apps = [Client1,Client2,Client3,Client4]
-  compose([apps])
+    apps = [Client1, Client2, Client3, Client4]
+    compose([apps])
+
 
 Client1.run(main())
