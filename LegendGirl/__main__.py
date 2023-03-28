d64f972afc4e7b70b946da15f39d4cda0cebfe8b
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-28 11:14:31 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,36 +1,4 @@
-from pyrogram import Client
+from . import start_botspam
 
-from LegendGirl.Config import *
-
-tokens = []
-for i in tokens:
-    tokens.append(BOT_TOKEN2)
-
-Client1 = Client(
-    "LegendSpam",
-    api_id=APP_ID,
-    api_hash=API_HASH,
-    bot_token=BOT_TOKEN,
-    plugins=dict(root="LegendGirl.LegendBoy"),
-)
-
-clients = [Client1]
-i = 0
-for token in tokens:
-    if ":" in token:
-        i = i + 1
-        Client2 = Client(
-            f"app{i}",
-            bot_token=f"{token}",
-            api_id=APP_ID,
-            api_hash=API_HASH,
-            plugins=dict(root="LegendGirl.LegendBoy"),
-        )
-        clients.append(Client2)
-
-
-async def main():
-    await compose(clients)
-
-
-Client1.run(main())
+if name == "main":
+    start_botspam()
