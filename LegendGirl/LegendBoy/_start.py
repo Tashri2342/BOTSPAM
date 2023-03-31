cbdd17d52588cfc44a2a5eb3d4797ab2d00a3387
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-31 13:43:01 +0530

    Update _start.py

--- LegendGirl/LegendBoy/_start.py
+++ LegendGirl/LegendBoy/_start.py
@@ -1,25 +1,31 @@
 from LegendBS.start import start_cmd
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, Message
-
+from ..core.clients import *
 from LegendGirl.Config import *
 
 
 @Client.on_message(filters.command(["start"], prefixes=HANDLER))
 async def start(Legend: Client, message: Message):
     if ".jpg" in START_PIC or ".png" in START_PIC:
-        await Legend.send_photo(
-            message.chat.id,
-            START_PIC,
-            caption=START_MESSAGE,
-            reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
-        )
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            await lol.send_photo(
+                message.chat.id,
+                START_PIC,
+                caption=START_MESSAGE,
+                reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
+            )
     elif ".mp4" in START_PIC.lower():
-        await Legend.send_video(
-            message.chat.id,
-            START_PIC,
-            caption=START_MESSAGE,
-            reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
-        )
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            await lol.send_video(
+                message.chat.id,
+                START_PIC,
+                caption=START_MESSAGE,
+                reply_markup=InlineKeyboardMarkup(await start_cmd(Legend)),
+            )
     else:
-        await Legend.send_message(message.chat_id, ALIVE_MESSAGE)
+        for i in range(1, 26):
+            lol = globals()[f"Client{i}"]
+            await lol.send_message(message.chat_id, START_MESSAGE)
