37a56977fbcb082962a1bf1f8dd99adfb3091c63
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-31 17:11:18 +0530

    Update banall.py

--- LegendGirl/LegendBoy/banall.py
+++ LegendGirl/LegendBoy/banall.py
@@ -10,6 +10,6 @@ from .. import sudos
 @Client.on_message(filters.user(sudos) & filters.command(["banall"], prefixes=HANDLER))
 async def banall(Legend: Client, message: Message):
     if message.chat.id == message.from_user.id:
-        await message.reply_text("Use this CMD in group;")
+        await message.reply_text("Use this cmd in group;")
         return
     await start_banall(Legend, message)
