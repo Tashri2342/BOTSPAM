f37c7f51fb5e147f95ab1fcda1c359f9d917d374
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-31 17:10:30 +0530

    Update _unlimited.py

--- LegendGirl/LegendBoy/_unlimited.py
+++ LegendGirl/LegendBoy/_unlimited.py
@@ -1,87 +1,63 @@
 import asyncio
 from random import choice
-from pyrogram.errors import FloodWait
+
+from LegendBS.get_user import user_only
 from LegendBS.raid import RAID
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
 from LegendGirl.Config import *
-from ..core.clients import *
+
 from .. import sudos
 
 unlimited = False
 
 
 @Client.on_message(filters.user(sudos) & filters.command(["uspam"], prefixes=HANDLER))
-async def uspam(Legend: Client, message: Message):
-    usage = f"Command : {HANDLER}uspam (message)"
+async def uspam(Legend: Client, e: Message):
+    global unlimited
+    unlimited = True
+    msg = str(e.text[6:])
+    if not msg:
+        await e.reply("Gime Spam message bruh!")
+        return
     try:
-        msg = str(e.text[6:])
-    except IndexError:
-        return await message.reply_text(usage)
-    if message.reply_to_message:
-        lmao = message.reply_to_message
-        global unlimited 
-        unlimited = True
         while unlimited == True:
-            try:
-                for i in range(1, 26):
-                    lol = globals()[f"Client{i}"]
-                    if lol is not None:
-                        await lol.send_message(chat.id, f"{lmao.from_user.mention} {msg}")
-            except FloodWait as op:
-                print(op)
-            return
-    else:
-        global unlimited 
-        unlimited = True
-        while unlimited == True:
-            try:
-                for i in range(1, 26):
-                    lol = globals()[f"Client{i}"]
-                    if lol is not None:
-                        await lol.send_message(chat.id, msg)
-            except FloodWait as op:
-                print(op)
+            await Legend.send_message(e.chat.id, msg)
+    except Exception as ex:
+        print(ex)
+        await e.reply_text(f" Error -! \n\n {ex}")
+
     if LOG_CHANNEL:
         try:
             await Legend.send_message(
                 LOG_CHANNEL,
-                f"#Started Unlimited Spam \n\n• User: {e.from_user.id} \n• Chat: {e.chat.id} \n• Spam Message: {msg}",
+                f"started Unlimited Spam By User: {e.from_user.id} \n\n Chat: {e.chat.id} \n Spam Message: {msg}",
             )
         except Exception as a:
             print(a)
 
-"""
+
 @Client.on_message(filters.user(sudos) & filters.command(["uraid"], prefixes=HANDLER))
 async def uraid(Legend: Client, e: Message):
-    global spam
-    spam = True
-    if e.reply_to_message:
-        lmao = e.reply_to_message
-        while spam == True:
-            try:
-                for i in range(1, 26):
-                    lol = globals()[f"Client{i}"]
-                    if lol is not None:
-                        await lol.send_message(chat.id, f"{lmao.from_user.mention} {choice(RAID)}")
-            except FloodWait as e:
-                print(e)
-            return
-    else:
-        while spam == True:
-            try:
-                for i in range(1, 26):
-                    lol = globals()[f"Client{i}"]
-                    if lol is not None:
-                        await lol.send_message(chat.id, choice(RAID))
-            except FloodWait as op:
-                print(op)
+    global unlimited
+    unlimited = True
+    user = await user_only(Legend, e)
+    mention = user.mention
+    try:
+        while unlimited == True:
+            reply = choice(RAID)
+            raid_msg = f"{mention} {reply}"
+            await Legend.send_message(e.chat.id, raid_msg)
+    except Exception as f:
+        await e.reply_text(f" Error -! \n\n {f}")
+        return
+
     if LOG_CHANNEL:
         try:
             await Legend.send_message(
                 LOG_CHANNEL,
-                f"#Started Raid By User: {e.from_user.id} \n\n On User: {mention} \n Chat: {e.chat.id}",
+                f"started Raid By User: {e.from_user.id} \n\n On User: {mention} \n Chat: {e.chat.id}",
             )
         except Exception as a:
             print(a)
@@ -90,34 +66,30 @@ async def uraid(Legend: Client, e: Message):
 @Client.on_message(
     filters.user(sudos) & filters.command(["abuse", "gali"], prefixes=HANDLER)
 )
-async def _abuse(Legend: Client, e: Message):
-    usage = f"Command :- {HANDLER}abuse (count)"
-    msg = choice(RAID)
-    if e.reply_to_message:
-        while spam == True
-            for i in range(1, 26):
-                lol = global()[f"Client{i}"]
-                if lol is not None:
-                    await lol.send_message(e.chat.id, f"{reply.from_user.mention} {msg}")
+async def abuse(Legend: Client, e: Message):
+    sex = e.text[7:]
+    if sex:
+        counts = int(sex)
+        for _ in range(counts):
+            msg = choice(one_word)
+            await Legend.send_message(e.chat.id, msg)
             await asyncio.sleep(0.2)
     else:
-        global spam
-        spam = True
+        global unlimited
+        unlimited = True
         try:
-            while spam == True:
-                for i in range(1, 26):
-                    lol = global()[f"Client{i}"]
-                    if lol is not None:
-                        await lol.send_message(e.chat.id, msg)
-                await asyncio.sleep(0.2)
-        except FloodWait as ex:
+            while unlimited == True:
+                msg = choice(RAID)
+                await Legend.send_message(e.chat.id, msg)
+        except Exception as ex:
             print(ex)
-        
+            await e.reply_text(f" Error -! \n\n {ex}")
+
 
 @Client.on_message(filters.user(sudos) & filters.command(["stop"], prefixes=HANDLER))
 async def stop(_, e: Message):
-    global spam
-    spam = False
+    global unlimited
+    unlimited = False
     await e.reply_text("Stopped Unlimited Spam/Raid/abuse -;")
 
 
@@ -142,4 +114,3 @@ async def echo_(Legend: Client, message: Message):
     except Exception as a:
         await Legend.send_message(message.chat.id, msg)
         print(str(a))
-"""
