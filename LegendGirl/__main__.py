2e2673b52de8977cee6e1c6ca09e1d188716a334
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-03-31 11:23:02 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -1,4 +1,21 @@
-from . import Start_BotSpam
+import platform 
 
-if __name__ == "__main__":
-    Start_BotSpam()
+from . import *
+from pyrogram import __version__ as py_version
+from pyrogram import idle
+
+from LegendBS.start_bot import start_bot
+def Start_BotSpam():
+    for i in range(1, 26):
+        var = globals()[f"Client{i}"]
+        if var is not None:
+            start_bot(var)
+    print("➖➖➖➖➖➖➖➖➖➖➖➖")
+    print(f"🔥 Bot Spam 🔥[INFO] : Group Username {group_username}")
+    print(f"🔥 Bot Spam 🔥[INFO] : Version - {platform.python_version()}")
+    print(f"🔥 Bot Spam 🔥[INFO]: SpamBot Version - {version}")
+    print(f"🔥 Bot Spam 🔥[INFO]: Pyrogram Version - {py_version}")
+    print("➖➖➖➖➖➖➖➖➖➖➖➖")
+    idle()
+
+asyncio.run(Start_BotSpam())
