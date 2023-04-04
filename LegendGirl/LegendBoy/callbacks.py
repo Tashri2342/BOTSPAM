b73b2a99c733469e2b9308fb2eb73161ca97399f
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-04 09:18:01 +0530

    Update callbacks.py

--- LegendGirl/LegendBoy/callbacks.py
+++ LegendGirl/LegendBoy/callbacks.py
@@ -2,7 +2,7 @@ from pyrogram import Client
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
 
 from Data import Data
-
+from LegendGirl.cmd_help import *
 
 @Client.on_callback_query()
 async def _callbacks(Legend: Client, callback_query: CallbackQuery):
@@ -16,7 +16,7 @@ async def _callbacks(Legend: Client, callback_query: CallbackQuery):
             await Legend.edit_message_text(
                 chat_id=chat_id,
                 message_id=message_id,
-                text="testing",
+                text="Help Menu One Powered By @TeamLegendXD",
                 reply_markup=InlineKeyboardMarkup(Data.HELP_MENU1),
             )
     elif query == "helpmenu2":
@@ -25,7 +25,7 @@ async def _callbacks(Legend: Client, callback_query: CallbackQuery):
         await Legend.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text="testing",
+            text="Help Menu Two Powered By @TeamLegendXD",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.HELP_MENU2),
         )
@@ -35,7 +35,11 @@ async def _callbacks(Legend: Client, callback_query: CallbackQuery):
         await Legend.edit_message_text(
             chat_id=chat_id,
             message_id=message_id,
-            text="testing",
+            text="Help Menu Two Powered By @TeamLegendXD",
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(Data.HELP_MENU3),
         )
+    elif query == "banall":
+        await callback_query.answer("banall", show_alert=True)
+    elif query == "birthday":
+        await callback_query.answer(birthday_help, show_alert=True)
