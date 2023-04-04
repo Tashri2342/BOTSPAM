8f23ec63daf6a7ff33eac06d4a6d17ff0049e34e
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-04 10:09:21 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -4,36 +4,36 @@ from pyrogram.types import InlineKeyboardButton
 class Data:
     HELP_MENU1 = [
         [
-            InlineKeyboardButton(text="⚜️ banall ⚜️, callback_data="banall"),
+            InlineKeyboardButton(text="⚜️ banall ⚜️", callback_data="banall"),
             InlineKeyboardButton(text="📍birthday 📍", callback_data="birthday"),
         ],
         [
-            InlineKeyboardButton(text="⚜️ Ping ⚜️, callback_data="core_help1"),
+            InlineKeyboardButton(text="⚜️ Ping ⚜️", callback_data="core_help1"),
             InlineKeyboardButton(text="📍 Restart 📍", callback_data="core_help2"),
         ],
         [
-            InlineKeyboardButton(text="⚜️ eval ⚜️, callback_data="evaluators_help1"),
+            InlineKeyboardButton(text="⚜️ eval ⚜️", callback_data="evaluators_help1"),
             InlineKeyboardButton(text="📍 exec 📍", callback_data="evaluators_help2"),
         ],
         [
-            InlineKeyboardButton(text="⚜️ Good Morning ⚜️, callback_data="gwish_help1"),
+            InlineKeyboardButton(text="⚜️ Good Morning ⚜️", callback_data="gwish_help1"),
             InlineKeyboardButton(text="📍Good Afternoon 📍", callback_data="gwish_help2"),
-            InlineKeyboardButton(text="⚜️ Good Night ⚜️, callback_data="gwish_help3"),           
+            InlineKeyboardButton(text="⚜️ Good Night ⚜️", callback_data="gwish_help3"),           
         ],
         [
-            InlineKeyboardButton(text="⚜️ Love Raid ⚜️, callback_data="lslove_help1"),
+            InlineKeyboardButton(text="⚜️ Love Raid ⚜️", callback_data="lslove_help1"),
             InlineKeyboardButton(text="📍Love Reply Raid📍", callback_data="lslove_help2"),
-            InlineKeyboardButton(text="⚜️ Love Dreply Raid ⚜️, callback_data="lslove_help3"),
+            InlineKeyboardButton(text="⚜️ Love Dreply Raid ⚜️", callback_data="lslove_help3"),
             InlineKeyboardButton(text="📍 List Raid 📍", callback_data="lslove_help4"),          
         ],
         [
-            InlineKeyboardButton(text="⚜️ Raid ⚜️, callback_data="raid_help1"),
+            InlineKeyboardButton(text="⚜️ Raid ⚜️", callback_data="raid_help1"),
             InlineKeyboardButton(text="📍Reply Raid📍", callback_data="raid_help2"),
-            InlineKeyboardButton(text="⚜️ Dreply Raid ⚜️, callback_data="raid_help3"),
+            InlineKeyboardButton(text="⚜️ Dreply Raid ⚜️", callback_data="raid_help3"),
             InlineKeyboardButton(text="📍 List Raid 📍", callback_data="raid_help4"),          
         ]
         [
-            InlineKeyboardButton(text="Close ", callback_data="close"),
+            InlineKeyboardButton(text="🔒 Close", callback_data="close"),
         ],
     ]
 
