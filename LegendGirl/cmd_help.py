55a56ab072d4c08a0514fc5aa99a2b9d6ead83ed
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-04 09:18:02 +0530

    Create cmd_help.py

---
+++ LegendGirl/cmd_help.py
@@ -0,0 +1,11 @@
+birthday_help = """📜 Intro :
+Spam The Chat With Birthday 
+
+🚩 Available Flag: 
+🔸 -u = Unlimited Spam
+
+👨‍💻 Usage :
+🔸 {HANDLER}birthday (counts)
+🔸 {HANDLER}birthday (counts) (reply to anyone)
+🔸 {HANDLER}birthday -u 
+🔸 {HANDLER}birthday -u (reply to anyone)"""
