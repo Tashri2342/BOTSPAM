13a3c53c64f9dcd562c89ccbd09cdc747452ce82
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-08 16:16:58 +0530

    Update __init__.py

--- LegendGirl/Config/__init__.py
+++ LegendGirl/Config/__init__.py
@@ -1,8 +1,10 @@
 import os
-
+from dotenv import load_dotenv
 ENV = bool(os.environ.get("ENV", False))
 
 if ENV:
     from sample_config import *  # noqa
 elif os.path.exists("config.py"):
     from config import *  # noqa
+elif os.path.exists(".env"):
+    load_dotenv(".env")
