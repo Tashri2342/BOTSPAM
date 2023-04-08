304edc88180dde978b2180663f1e9ce60018e404
LEGEND-AI <Krishna045Jaiswal@gmail.com>
2023-04-08 11:44:49 +0000

    legend fixed

--- start.py
+++ start.py
@@ -1,7 +1,8 @@
-import os, sys
-
+import os
+import sys
 
 os.system("clear")
+
 
 def bot_start():
     os.system("clear")
@@ -18,38 +19,38 @@ def bot_start():
 
 
 def check_again():
-    recheck= input(f"\nHave You Filled ALL Vars Correctly?: y/n: ")
+    recheck = input(f"\nHave You Filled ALL Vars Correctly?: y/n: ")
     if recheck.lower() == "n":
         os.system("clear")
         print(f"Ohh! Now Fill Your Vars Again")
-    elif recheck.lower() == "y":       
+    elif recheck.lower() == "y":
         get_start()
     else:
         print(f"\n{ask}Input Must Be Y or N")
         check_again()
 
+
 botspam = input(f"{ask}Want to fill vars ? if yes type Y/yes else press enter: ")
-if botspam.lower() in ['y', 'yes']:
-  if not os.path.exists(".env"):
-    y = open(".env", "w")
-    y.write(vars)
-    y.close()
-    os.system("clear")
-  if os.path.exists(".env"):
-    f = open(".env")
-    check = f.read()
-    print(check)   
-    x.close()
-    check_again()
-    if not len(lines) == 35:
-        os.system("rm -rf .env")
+if botspam.lower() in ["y", "yes"]:
+    if not os.path.exists(".env"):
         y = open(".env", "w")
         y.write(vars)
         y.close()
         os.system("clear")
-    else:
-        os.system("clear")
+    if os.path.exists(".env"):
+        f = open(".env")
+        check = f.read()
+        print(check)
+        x.close()
+        check_again()
+        if not len(lines) == 35:
+            os.system("rm -rf .env")
+            y = open(".env", "w")
+            y.write(vars)
+            y.close()
+            os.system("clear")
+        else:
+            os.system("clear")
 else:
-  os.system("clear")
-  os.system("python3 -m LegendGirl")
-
+    os.system("clear")
+    os.system("python3 -m LegendGirl")
