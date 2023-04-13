f3943c5fbb0f49437439d17a973cce2d51b507dc
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-13 22:49:55 +0530

    Update __init__.py

--- LegendGirl/__init__.py
+++ LegendGirl/__init__.py
@@ -1,11 +1,29 @@
 import time
-
+import os, glob
 from .Config import *
 from .core import *
 
 version = "v1"
 group_username = "@LegendBotSpam"
 start_time = time.time()
+
+
+
+async def all_plugins():
+    # This generates a list of plugins in this folder for the * in __main__ to
+    # work.
+    from glob import glob
+    from os.path import basename, dirname, isfile
+
+    mod_paths = glob(dirname(__file__) + "/*.py")
+    all_plugs = [
+        basename(f)[:-3]
+        for f in mod_paths
+        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+    ]
+    return sorted(all_plugs)
+
+
 # Sudo Users
 sudos = []
 # full debugging
