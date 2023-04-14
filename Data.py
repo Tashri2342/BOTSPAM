6c52f933ca93f63cdd419bdf7658e354fe2182ab
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-15 00:41:52 +0530

    Update Data.py

--- Data.py
+++ Data.py
@@ -25,18 +25,6 @@ class Data:
             InlineKeyboardButton(text="⚜️ Good Night ⚜️", callback_data="gwish_help3"),
         ],
         [
-            InlineKeyboardButton(text="⚜️ Love Raid ⚜️", callback_data="lslove_help1"),
-            InlineKeyboardButton(
-                text="📍Love Reply Raid📍", callback_data="lslove_help2"
-            ),
-        ],
-        [
-            InlineKeyboardButton(
-                text="⚜️ Love Dreply Raid ⚜️", callback_data="lslove_help3"
-            ),
-            InlineKeyboardButton(text="📍 List Raid 📍", callback_data="lslove_help4"),
-        ],
-        [
             InlineKeyboardButton(text="⚜️ Raid ⚜️", callback_data="raid_help1"),
             InlineKeyboardButton(text="📍Reply Raid📍", callback_data="raid_help2"),
         ],
@@ -66,7 +54,7 @@ class Data:
         ],
         [
             InlineKeyboardButton(
-                text="⚜️ Dreply Raid ⚜️", callback_data="unlimited_help5"
+                text="⚜️ Echo ⚜️", callback_data="unlimited_help5"
             ),
         ],
         [
