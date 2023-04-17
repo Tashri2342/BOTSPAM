79c670a154b1731113717097980a286b6716dc8f
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-17 07:35:22 +0530

    Update help.py

--- LegendGirl/LegendBoy/help.py
+++ LegendGirl/LegendBoy/help.py
@@ -24,7 +24,7 @@ async def _help(Legend: Client, message: Message):
         )
     else:
         await Legend.send_message(
-            message.chat_id,
+            message.chat.id,
             HELP_MSG,
             reply_markup=InlineKeyboardMarkup(Data.HELP_MENU1),
         )
