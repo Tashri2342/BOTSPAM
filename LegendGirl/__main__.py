c43b6b2964ca5de36b544c1bdc154d7189683641
Krishna-45Jaiswal <127404814+Krishna-45Jaiswal@users.noreply.github.com>
2023-04-29 14:05:12 +0530

    Update __main__.py

--- LegendGirl/__main__.py
+++ LegendGirl/__main__.py
@@ -5,23 +5,12 @@ from LegendBS.start_bot import start_bot
 from pyrogram import __version__ as py_version
 from pyrogram import idle
 
-from . import *
+from .core.clients import *
 
 loop = asyncio.get_event_loop()
 
 
-async def Start_BotSpam():
-    for i in range(1, 26):
-        var = globals()[f"Client{i}"]
-        if var is not None:
-            await start_bot(var)
-    print("➖➖➖➖➖➖➖➖➖➖➖➖")
-    print(f"🔥 Bot Spam 🔥[INFO] : Group Username {group_username}")
-    print(f"🔥 Bot Spam 🔥[INFO] : Version - {platform.python_version()}")
-    print(f"🔥 Bot Spam 🔥[INFO]: SpamBot Version - {version}")
-    print(f"🔥 Bot Spam 🔥[INFO]: Pyrogram Version - {py_version}")
-    print("➖➖➖➖➖➖➖➖➖➖➖➖")
-    await idle()
+
 
 
 if __name__ == "__main__":
