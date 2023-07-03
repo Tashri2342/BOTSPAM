985468d1116551b2263252929cb69a83251a06c4
Tashri2342 <108603052+Tashri2342@users.noreply.github.com>
2023-07-03 07:52:11 +0530

    Update help.py

--- LegendGirl/LegendBoy/help.py
+++ LegendGirl/LegendBoy/help.py
@@ -12,7 +12,7 @@ else:
 
 @Client.on_message(filters.command(["help"], prefixes=HANDLER))
 async def _help(Legend: Client, message: Message):
-    HELP_MSG = "Help Menu Powered By @TeamLegendXD"
+    HELP_MSG = "Help Menu Powered By @Tashribots2342"
     if ".jpg" in HELP_PIC or ".png" in HELP_PIC:
         await Legend.send_photo(
             message.chat.id,
